73580d332a98fcb12644e425d20fd8698aa1b109
Николай <kolaygreha@rambler.ru>
2020-05-21 00:35:12 +0300

    5

--- app/src/main/java/com/example/managetime/AddTaskActivity.java
+++ app/src/main/java/com/example/managetime/AddTaskActivity.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class AddTaskActivity extends AppCompatActivity {
+
+    FloatingActionButton addTaskFloatingButton = findViewById(R.id.addButton);
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
