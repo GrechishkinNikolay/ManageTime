b116bae4a36916cc14bd777335b485cebf14627f
Николай <kolaygreha@rambler.ru>
2020-05-20 19:13:39 +0300

    2

---
+++ app/src/main/java/com/example/managetime/AddTaskActivity.java
@@ -0,0 +1,14 @@
+package com.example.managetime;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTaskActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
